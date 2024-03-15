6c5333d384ab5e650e1f07a6d1df84dbab9a26fd
sarow <msarowar947@gmail.com>
2024-03-16 00:00:29 +0600

    changed

--- bin/Live_Test.dart
+++ bin/Live_Test.dart
@@ -1,11 +1,25 @@
-void main(){
+void main() {
+  print('Original Fruit Details before Discount:');
+  List<Map<String, dynamic>> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': ' Purple', 'price': 3.0}
+  ];
+  displayFruitDetails(fruits);
+  applyPriceDiscount(fruits, 10);
+  displayFruitDetails(fruits);
+}
+void displayFruitDetails(List<Map<String, dynamic>> fruits) {
 
-  print('i am md sarowar');
-  print('hello world');
-  List<String>city=['Dhaka','Rajshahi','Naogaon','all city in bangladesh'];
-  // code is good but me very bad boy or man not good ;
-  // sjpoowp;
-  //slkjks
-  // lkjsppokl;ap;lkj; alkj;lkjj ;lkajlkj ;lkj;lkjj ljk;lk;;;k;
-
+  for (var fruit in fruits) {
+    print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}');
+  }
+}
+void applyPriceDiscount(List<Map<String, dynamic>> fruits, double discountPercentage) {
+  for (var fruit in fruits) {
+    double currentPrice = fruit['price'];
+    double discountAmount = currentPrice * (discountPercentage / 100);
+    fruit['price'] = (currentPrice - discountAmount).toStringAsFixed(2);
+  }
+  print('Fruit Details After Applying 10% Discount:');
 }
